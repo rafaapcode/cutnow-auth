b4bbd86b5274a98116a5fed9ca5cc70fa5457b43
rafaapcode <rafaap2003@gmail.com>
2024-08-17 19:57:57 -0300

    fix: fizing an error of dependency

--- src/barbershop/barbershop.module.ts
+++ src/barbershop/barbershop.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { DatabaseModule } from 'src/database/database.module';
 import { BarbershopController } from './barbershop.controller';
 import { BarbershopService } from './barbershop.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.getOrThrow('JWT_SECRET'),
+        signOptions: { expiresIn: '2h' },
+      }),
+    }),
+  ],
   controllers: [BarbershopController],
   providers: [BarbershopService],
 })
