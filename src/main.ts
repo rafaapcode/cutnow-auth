d6172423d59629551cbdd0608af04a92f5c291c9
rafaapcode <rafaap2003@gmail.com>
2024-11-05 08:48:59 -0300

    feat: creating a CORS config

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    origin: '*',
+    origin: 'https://cutnow-frontend.vercel.app',
   });
   app.use(cookieParser());
   await app.listen(process.env.PORT);
